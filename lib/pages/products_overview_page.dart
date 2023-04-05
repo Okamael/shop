1963b494fd8809620e1398a3dd7757d339767666
Murilo Ramos da Silva <murilo.rsilva@totvs.com.br>
2023-04-05 18:12:08 -0300

    Implementação do

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverViewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
   ProductsOverViewPage({super.key});
 
   @override
@@ -13,17 +10,7 @@ class ProductsOverViewPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10),
-        itemBuilder: (context, index) =>
-            ProductItem(product: loadedProducts[index]),
-      ),
+      body: ProductGrid(),
     );
   }
 }
