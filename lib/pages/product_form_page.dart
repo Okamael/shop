c18125296ea1b02545730c38cb99bdd6dfa09b4f
Murilo Ramos <murilo-bmx_inor@hotmail.com>
2023-04-10 22:45:00 -0300

    adicionando

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -9,13 +13,49 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocous = FocusNode();
-  final _descriptionFocous = FocusNode();
+  final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
+  final _formData = Map<String, Object>();
   @override
   void dispose() {
     super.dispose();
     _priceFocous.dispose();
-    _descriptionFocous.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  void updateImage() {
+    setState(() {});
+  }
+
+  void _submitForm() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState?.save();
+    final newProduct = Product(
+        id: Random().nextDouble().toString(),
+        name: _formData['name'] as String,
+        description: _formData['description'] as String,
+        price: _formData['price'] as double,
+        imageUrl: _formData['imageUrl'] as String);
+
+    print(newProduct.id);
+    print(newProduct.name);
+    print(newProduct.imageUrl);
   }
 
   @override
@@ -23,39 +63,78 @@ class _ProductFormPageState extends State<ProductFormPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de produtos'),
+        actions: [IconButton(onPressed: _submitForm, icon: Icon(Icons.save))],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+            key: _formKey,
             child: ListView(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Nome'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (value) {
-                FocusScope.of(context).requestFocus(_priceFocous);
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Preço'),
-              textInputAction: TextInputAction.next,
-              focusNode: _priceFocous,
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
-              ),
-              onFieldSubmitted: (value) {
-                FocusScope.of(context).requestFocus(_descriptionFocous);
-              },
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Descrição'),
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.multiline,
-              maxLines: 3,
-              focusNode: _descriptionFocous,
-            ),
-          ],
-        )),
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_priceFocous);
+                  },
+                  onSaved: (name) => _formData['name'] = name ?? '',
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Preço'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _priceFocous,
+                  onSaved: (price) =>
+                      _formData['price'] = double.parse(price ?? '0'),
+                  keyboardType: TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_descriptionFocus);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.multiline,
+                  onSaved: (description) =>
+                      _formData['description'] = description ?? '',
+                  maxLines: 3,
+                  focusNode: _descriptionFocus,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Url da imagem'),
+                        onSaved: (imageurl) =>
+                            _formData['imageUrl'] = imageurl ?? '',
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        focusNode: _imageUrlFocus,
+                        controller: _imageUrlController,
+                        onFieldSubmitted: (value) => _submitForm(),
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      margin: EdgeInsets.only(top: 10, left: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                      ),
+                      alignment: Alignment.center,
+                      child: _imageUrlController.text.isEmpty
+                          ? Text('Informe a URL')
+                          : FittedBox(
+                              child: Image.network(_imageUrlController.text),
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
       ),
     );
   }
